1ca7798aad84ea3769c6960fbaa84604bc3b4205
armasperezoriana <78425776+armasperezoriana@users.noreply.github.com>
2021-02-04 14:06:32 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+  define('URL', 'http://localhost/ut/');
+
+  define('HOST', 'localhost');
+  define('DB', 'mysql');
+  define('DBNAME', 'ut');
+  define('USER', 'admin');
+  define('PASS', 'admin');
+  define('CHARSET', 'utf8');
+ 
+?>
